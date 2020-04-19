3696c4e7df5f05f7ee1ec7b62c4b3ef3aaba097a
Itamar Soffer <itamar.soffer@gmail.com>
2020-04-19 16:47:38 +0300

    starting first react navite app

---
+++ EventList.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+class EventList extends Component {
+
+render(){
+	return(
+	<Text> Hello Sofferico!</Text>
+);
+}
+}
+
+
+export default EventList;
