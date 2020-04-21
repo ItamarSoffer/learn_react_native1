d63d0e0fabdae67998d5616fd81715fba123f851
Itamar Soffer <itamar.soffer@gmail.com>
2020-04-21 11:15:23 +0300

    starts adding text form

--- EventForm.js
+++ EventForm.js
@@ -3,7 +3,25 @@ import {
 	View,
 	Text, 
 	TouchableHighlight,
+	TextInput,
+	StyleSheet,
 } from 'react-native';
+
+const styles = StyleSheet.create({
+	fieldContainer: {
+		marginTop: 20,
+		marginBottom: 20,
+		backgroundColor: "#fff",
+	},
+	text: {
+		height: 40,
+		margin: 0,
+		marginRight: 7,
+		paddingLeft: 10,
+	}
+
+
+});
 
 
 class EventForm extends Component{
@@ -14,7 +32,19 @@ handleAddPress = () => {
 
 	render() {
 		return (
-			<View>
+			<View
+				style = {{
+					flex:1
+				}}
+
+			>
+			<View style={styles.fieldContainer}> 
+				<TextInput
+				style={styles.text}
+				placeholder=" Event title"
+				spellCheck={false}
+				/>
+			</View>
 				<TouchableHighlight
 				onPress={this.handleAddPress}>
 				<Text>Add </Text>
